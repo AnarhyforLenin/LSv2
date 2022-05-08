28eeda16af62e73b88d54a4705ac23c7c64df0ed
AnarhyforLenin <91470101+AnarhyforLenin@users.noreply.github.com>
2022-05-08 03:46:34 +0300

    vvv

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,259 @@
+package com.example.myapplication;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.tomer.fadingtextview.FadingTextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private View layoutEncontrarPers;
+    private ImageView imgAnim, imgAnim2, imgAnim3;
+    private Handler handlerAnimationCIMG;
+    private TextView textView;
+    private TextView tip;
+    private TextView timerTextView;
+    private FadingTextView fadingTextView;
+    private BreathingAnimationThread breathingAnimationThread;
+    private boolean isRunning = false;
+    private boolean isTapped = false;
+    Timerthread timerthread;
+    Timer timer;
+
+    private void setUpFadeAnimation(final TextView textView) {
+        final Animation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(0);
+        fadeIn.setStartOffset(0);
+        final Animation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeOut.setDuration(0);
+        fadeOut.setStartOffset(0);
+
+        fadeIn.setAnimationListener(new Animation.AnimationListener(){
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                textView.startAnimation(fadeOut);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+        });
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener(){
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                textView.startAnimation(fadeIn);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+        });
+
+        textView.startAnimation(fadeOut);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        timerTextView = findViewById(R.id.taimer);
+
+        findViewById(R.id.buttonStart).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+
+                if(timerthread == null) {
+                    startTask();
+
+
+                }
+                else {
+                    imgAnim.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(1000);
+                    imgAnim2.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(1000);
+                    imgAnim3.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(1000);
+                    //tip.setVisibility(View.GONE);
+                    stopTask();
+                }
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 60000);
+            }
+
+        });
+
+
+
+
+    }
+    private void init() {
+        this.handlerAnimationCIMG = new Handler();
+        this.layoutEncontrarPers = findViewById(R.id.layoutPers);
+        this.imgAnim = findViewById(R.id.Anim3);
+        this.imgAnim2 = findViewById(R.id.Anim1);
+        this.imgAnim3 = findViewById(R.id.Anim2);
+        this.tip=findViewById(R.id.tip);
+
+    }
+
+    private void startTask() {
+        timerthread = new Timerthread(timerTextView);
+        timerthread.start();
+    }
+
+    private void stopTask() {
+        if(timerthread != null) {
+            timerthread.stopAnimation();
+            timerthread = null;
+        }
+    }
+
+
+
+
+
+
+    class Timerthread extends Thread{
+        TextView tv;
+        int var = MainActivity2.var;
+        String name = MainActivity2.name;
+        boolean isRunning=false;
+        String[] seq = {"4","3","2","1"};
+        Timerthread(TextView tv){
+            this.tv=tv;
+        }
+        public void stopAnimation() {
+            isRunning = false;
+            tip.setVisibility(View.GONE);
+        }
+        public void run(){
+            final int[] firstTimer = {1};
+            isRunning=true;
+            while (true){
+                for(final String t : seq){
+                    if(!isRunning) {
+                        firstTimer[0]=1;
+
+                        break;
+                    }
+                    if(t.equals("4")&& firstTimer[0] ==1) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tip.setVisibility(View.VISIBLE);
+                                imgAnim.animate().scaleX(1.5f).scaleY(1.5f).alpha(1f).setDuration(4000);
+                                imgAnim2.animate().scaleX(2f).scaleY(2f).alpha(0.5f).setDuration(4000);
+                                imgAnim3.animate().scaleX(3f).scaleY(3f).alpha(0.5f).setDuration(4000);
+                                if (var ==10) {tip.setText(name+",сделайте вдох");}
+                                if (var ==11) {tip.setText(name+",сделай вдох");}
+                                if (var ==12) {
+                                    tip.setText("Солнце, сделай глубокий вдох");
+                                }
+
+                                firstTimer[0] +=1;
+                            }
+                        });
+
+                    }
+
+                    if(t.equals("4")&& firstTimer[0] ==2) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgAnim.animate().scaleX(1.5f).scaleY(1.5f).alpha(1f).setDuration(4000);
+                                imgAnim2.animate().scaleX(2f).scaleY(2f).alpha(0.5f).setDuration(4000);
+                                imgAnim3.animate().scaleX(3f).scaleY(3f).alpha(0.5f).setDuration(4000);
+                                if (var ==10) {tip.setText(name+",задержите дыхание");}
+                                if (var ==11) {tip.setText(name+",задержи дыхание");}
+                                if (var ==12) {tip.setText("Солнце, задержи дыхание");}
+                                firstTimer[0] +=1;
+                            }
+                        });
+
+                    }
+                    if(t.equals("4")&& firstTimer[0] ==3) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgAnim.animate().scaleX(1f).scaleY(1f).alpha(0f).setDuration(4000);
+                                imgAnim2.animate().scaleX(1f).scaleY(1f).alpha(0f).setDuration(4000);
+                                imgAnim3.animate().scaleX(1f).scaleY(1f).alpha(0f).setDuration(4000);
+                                if (var ==10) {tip.setText(name+",сделайте выдох");}
+                                if (var ==11) {tip.setText(name+",сделай выдох");}
+                                if (var ==12) {tip.setText("Солнце, сделай выдох");}
+                                firstTimer[0]+=1;
+                            }
+                        });
+
+                    }
+                    if(t.equals("4")&& firstTimer[0] ==4) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imgAnim.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(4000);
+                                imgAnim2.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(4000);
+                                imgAnim3.animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(4000);
+                                if (var ==10) {tip.setText(name+",задержите дыхание");}
+                                if (var ==11) {tip.setText(name+",задержи дыхание");}
+                                if (var ==12) {tip.setText("Солнце, задержи дыхание");}
+                                firstTimer[0]=1;
+                            }
+                        });
+                    }
+
+                    tvSetText(t, tv);
+                    try{Thread.sleep(1000); } catch(Exception ex){}
+                }
+
+            }
+        }
+
+
+    }
+
+    private void tvSetText(String text, TextView tv) {
+        runOnUiThread(new Runnable() { @Override public void run() { tv.setText(text); } });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/Tamagochi.java
@@ -0,0 +1,91 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ObjectInputStream;
+import java.text.BreakIterator;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Tamagochi extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tamagochi);
+
+        TextView textView = findViewById(R.id.test);
+        Calendar c = Calendar.getInstance();
+        //String time2 = String.format("%2d" , c.get(Calendar.HOUR_OF_DAY))+":"+String.format("%02d" , c.get(Calendar.MINUTE));
+
+        SimpleDateFormat df1 = new SimpleDateFormat("hh:mm a");
+        String strdate = df1.format(c.getTime());
+
+        String st = strdate.replace(":","");
+        String st1 = st.replace(" ","");
+        String st2 = st1.substring(0,4);
+        String substr1 = strdate.substring(6,8);
+
+        int foo = Integer.parseInt(st2);
+
+        String prayerTimes1 = "AM";
+        int prayerTimes12 = 500;
+
+        String prayerTimes2 = "PM";
+        int prayerTimes22 = 900;
+
+        if((substr1.equals(prayerTimes1) && foo<prayerTimes12) || (substr1.equals(prayerTimes2) && foo>prayerTimes22))
+        {
+            textView.setText("st2");
+        }
+
+        final int[] time = {100};
+        TextView textHealth = (TextView)findViewById(R.id.health);
+        ProgressBar progressHealth = (ProgressBar)findViewById(R.id.vertical_progressbar1);
+        TextView textEat = (TextView)findViewById(R.id.eat);
+        ProgressBar progressEat = (ProgressBar)findViewById(R.id.vertical_progressbar2);
+        TextView textHappy = (TextView)findViewById(R.id.happyness);
+        ProgressBar progressHappy = (ProgressBar)findViewById(R.id.vertical_progressbar3);
+
+        new CountDownTimer(100000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textHealth.setText(checkDigit(time[0]));
+                progressHealth.setProgress(time[0]);
+                textEat.setText(checkDigit(time[0]));
+                progressEat.setProgress(time[0]);
+                textHappy.setText(checkDigit(time[0]));
+                progressHappy.setProgress(time[0]);
+                time[0]--;
+            }
+
+            private String checkDigit(int time) {
+                return String.valueOf(time);
+            }
+
+            public void onFinish() {
+                textHealth.setText("0");
+                textEat.setText("0");
+                textHappy.setText("0");
+            }
+
+        }.start();
+
+
+
+
+    }
+}
